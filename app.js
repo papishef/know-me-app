5cbaca2c82168f1c63fe7e6a784ee18cfd8d9016
papishef <sheriff.malebo@gmail.com>
2020-03-30 20:05:50 +0100

    Server Build

---
+++ app.js
@@ -0,0 +1,27 @@
+//jshint esversion: 6
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const cookieParser = require("cookie-parser");
+
+
+
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(req, res) {
+  console.log("Server is listening on port " + port);
+});
