9e3840559dee4e224f5a1f0109f6fb81c5d18b9c
papishef <sheriff.malebo@gmail.com>
2020-03-31 12:34:05 +0100

    server and client grouping

---
+++ questions.js
@@ -0,0 +1,164 @@
+///////////////////THIS FILE CONTAIN ALL THE QUESTIONS////////////////////////////
+
+
+
+const allQuestions = {
+  one: {
+    id: 1,
+    q: "If a genie could grant your most important wish, what would it be?",
+    category: "personal"
+  },
+  two: {
+    id: 2,
+    q: "What is your greatest fear?",
+    category: "personal"
+  },
+  three: {
+    id: 3,
+    q: "If you were to die for someone, who would it be?",
+    category: "personal"
+  },
+  four: {
+    id: 4,
+    q: "What do you do?",
+    category: "Professional"
+  },
+  five: {
+    id: "5",
+    q: "Who is that one person you can tell anything?",
+    category: "personal"
+  },
+  six: {
+    id: "6",
+    q: "What is your full name?",
+    category: "personal"
+  },
+  seven: {
+    id: "7",
+    q: "Have you ever smoked?",
+    category: "social"
+  },
+  eight: {
+    id: "8",
+    q: "Have you done oral sex before?",
+    category: "sexual"
+  },
+  nine: {
+    id: "9",
+    q: "Do you love quickies?",
+    category: "sexual"
+  },
+  ten: {
+    id: "10",
+    q: "Have you ever smoked?",
+    category: "social"
+  },
+  eight: {
+    id: "8",
+    q: "Have you done oral sex before?",
+    category: "sexual"
+  },
+  9.	 (sexual)
+  10.	What is your birth date. (casual)
+  11.	What is your favorite sex position? (sexual)
+  12.	If you had to travel far away for a long period, where would you rather go? (casual)
+  13.	What is your current relationship status? (social)
+  14.	Do you think it is necessary to reveal your past to your current boyfriend/girlfriend? (social)
+  15.	Do you prefer romance novels to adventures? (casual)
+  16.	Have you ever done hard drugs? (social)
+  17.	When was the last time you cried? (casual)
+  18.	Do you have a female best friend or a male best friend? (casual)
+  19.	Are you bi-sexual or heterosexual? (sexual)
+  20.	What part of your body do you think mostly attracts the opposite sex to you? (sexual)
+  21.	Are you an introvert or an extrovert? (social)
+  22.	What is your longest sex duration? (sexual)
+  23.	Between sex and romance, which do you enjoy most? (sexual)
+  24.	If I asked politely, would you watch porn with me? (sexual)
+  25.	What is your genre of music? (casual)
+  26.	Do you prefer having sex with condoms or without condoms? (sexual)
+  27.	What is your most embarrassing moment? (casual)
+  28.	If you had the opportunity to change anything about yourself, what would it be? (casual)
+  29.	What is your worst mistake? (casual)
+  30.	Are you a screamer or a moaner during sex? (sexual)
+  31.	What is that one thing you are very proud of? (casual)
+  32.	What are you most grateful for? (casual)
+  33.	Which would you rather prefer for relaxation; a book or movie? (casual)
+  34.	Have you ever thought of committing suicide? (Psychological)
+  35.	What is your favorite Television show? (casual)
+  36.	What are you putting on at the moment? (sexual)
+  37.	Have you ever survived a life-threatening disease? (Psychological)
+  38.	Do you prefer a light-skinned or a dark-skinned guy/girl? (casual)
+  39.	Can you reveal all of your sexual history to your boyfriend/girlfriend? (sexual)
+  40.	What is your major weakness? (casual)
+  41.	Have you ever gone back to an ex? (social)
+  42.	What is your G-spot? (sexual)
+  43.	Has your heart ever been broken? (social)
+  44.	Can you give a blowjob if your boyfriend/ girlfriend asks for it? (sexual)
+  45.	What is your ideal man/woman? (social)
+  46.	Have you ever asked a guy out? (social)
+  47.	How do you handle rejection? (social)
+  48.	What is your position on premarital sex? (social)
+  49.	What is your deal breaker in a relationship? (social)
+  50.	Would you forgive a cheating partner? (social)
+  51.	Where is the weirdest place you have ever had sex? (sexual)
+  52.	Have you ever had an abusive partner? (social)
+  53.	What advice would you give to your younger self? (casual)
+  54.	Have you ever been sexually abused? (Psychological)
+  55.	Have your ever had a near-death experience? (Psychological)
+  56.	What  is your worst fear (casual)
+  57.	What is your most memorable moment yet? (casual)
+  58.	If you had a pet, what would it be? (casual)
+  59.	Are you a virgin? (sexual)
+  60.	Between attention, sex and money, what do you crave most from a guy? ( social)
+  61.	At what age did you have your first sex? (sexual)
+  62.	What are you fondly called? (casual)
+  63.	Does having sex on a first date make you loose? (sexual)
+  64.	Have you ever cheated on your boyfriend/ girlfriend? (social)
+  65.	Have you ever had a one-night stand? (social)
+  66.	Are you good at playing games? (casual)
+  67.	Do you have a celebrity crush? ( casual)
+  68.	Do you have a deep secret you can never ever let out? (casual)
+  69.	What is that one bad habit you are addicted to? (social)
+  70.	What is that one thing you cherish most in a guy/ girl? (casual)
+  71.	What is your zodiac sign? ( casual)
+  72.	What is your bra size? (Personal)
+  73.	If you were an animal, which would you rather be? (casual)
+  74.	What is the duration of your longest relationship? (casual)
+  75.	Have you ever ever dated a person you never loved? (casual)
+  76.	What is the most daring thing you have ever done? (casual)
+  77.	What is your favorite number? (casual)
+  78.	Have you ever crushed on your friend’s boyfriend/girlfriend? (casual)
+  79.	How long can you stay without having sex? (sexual)
+  80.	What is that one thing you can’t  live without?(casual)
+  81.	Can you stay in a room for a month with someone you love without sexually touching him/ her?
+  82.	If you were to live without sex or money for the rest of your life,which would you rather choose?  (social/sexual)
+  83.	Money, power, fame and sex, pick one. (Social/sexual)
+  84.	Would you date your friend’s ex?
+  85.	How old are you? (casual)
+  86.	What is fulfillment for you? (casual)
+  87.	Can you initiate sex? (sexual)
+  88.	What are your hobbies? (casual)
+  89.	What is your fondest memory? (casual)
+  90.	Do you watch porn? (sexual)
+  91.	Have you ever sent your nude picture? (sexual)
+  92.	Are you dominant in bed? (sexual)
+  93.	What is your favourite food? (casual)
+  94.	What is your saddest moment? (casual)
+  95.	Do you like being explored sexually? (sexual)
+  96.	What turns you on? (sexual)
+  97.	Are you a good kisser? (sexual)
+  98.	How many rounds of sex can you go? (sexual)
+  99.	What is your favourite spot to be kissed?(sexual)
+  100.	Do you like PDA? (casual)
+  101.	Have you ever masturbated? (sexual)
+  102.	Has a stranger seen your boobs/dick mistakenly? (sexual)
+  103.	What is your sexual fantasy? (sexual)
+  104.	Have you ever done “friends with benefits”? (casual)
+  105.	If you could have sex with a celebrity, who would it be? (sexual)
+  106.	What are your special talents? (casual)
+  107.	Have you ever wished you could start life all over again? (casual)
+  108.	What is your birth date? (casual)
+
+}
+
+module.exports = allQuestions;
