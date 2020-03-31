8faa8e030da1cc34cff27fa75c09389b61a0639a
veectorh <blackjetv5@gmail.com>
2020-04-01 00:28:16 +0100

    updated styling components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
-import Onboarding from './views/Onboarding'
+import Onboarding from './views/Onboarding';
+import SignIn from './views/SignIn'
 
 function App() {
   return (
     <div className="App">
-      <Onboarding />
+      <Router>
+      <Switch>
+          <Route path="/onboard">
+            <Onboarding />
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          {/* <Route path="/">
+            <Home />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
