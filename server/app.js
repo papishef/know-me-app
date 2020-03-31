e65d2c1d3548cc80642b539c490d34fe7e505868
papishef <sheriff.malebo@gmail.com>
2020-03-31 19:02:13 +0100

    questions defined

--- server/app.js
+++ server/app.js
@@ -9,8 +9,31 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const cookieParser = require("cookie-parser");
 
+const allQuestions = require("./questions");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//To allow our express serveer render local files including css and javascript
+
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 600000
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+app.get("/", (req, res) => {
+  res.send(allQuestions.eightyEight.q);
+});
 
 
 
