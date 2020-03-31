345f2269ced4869e97278201339def5b4a7cbbc4
veectorh <blackjetv5@gmail.com>
2020-03-31 23:27:23 +0100

    cleared boilerplate and added onboarding page

--- client/src/views/Onboarding.js
+++ client/src/views/Onboarding.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import OnboardingImg from '../assets/Onboarding.png'
 
 export default function Onboarding() {
     return (
         <div>
-            
+            <img className='onboard' src={OnboardingImg} alt='#' />
         </div>
     )
 }
