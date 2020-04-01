c7a8a6abfc5b8107bf578d3fc0e6bdedb52fddc7
veectorh <blackjetv5@gmail.com>
2020-04-01 09:48:45 +0100

    added bootstrap to react and created signin template. unstyled

---
+++ client/src/views/SignIn.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Logo from '../assets/playroom-logo.png'
+import { Container, Input, InputGroup, InputGroupAddon, InputGroupText, Button } from 'reactstrap'
+
+export class SignIn extends Component {
+    render() {
+        return (
+            <div className='login-page-wrapper'>
+                <Container className="themed-container" >
+                    <div><img className='logo pt-4' src={Logo} alt='#' /></div>
+                    <div className='pt-4'>
+                        <InputGroup>
+                            <Input className='bg-transparent text-white form-input' placeholder="Nickname" />
+                        </InputGroup>
+                        <br />
+                        <InputGroup>
+                            <Input className='bg-transparent text-white form-input' placeholder="sex" />
+                        </InputGroup>
+                        {/* <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                                <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+                            </InputGroupText>
+                            </InputGroupAddon>
+                        </InputGroup> */}
+                        <br />
+                        <Button className='submit border border-light' color="secondary">secondary</Button>{' '}
+                    </div>
+                    <div className='text-white'>
+                        <strong>Disclaimer:</strong>Nickname should be kept private,
+                         it would be used to identify you when you log in and out of the game.
+                    </div>
+                    <div className='text-white'>
+                        By submiting, I agree to your <strong>Terms of service</strong> and
+                        <strong>Privacy Policy</strong>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default SignIn
