890c5dea6192a64097012b4ef2cde717fd16b501
veectorh <blackjetv5@gmail.com>
2020-04-01 19:37:56 +0100

    invitation view style update

--- client/src/views/Invitation.js
+++ client/src/views/Invitation.js
@@ -5,13 +5,14 @@ import Facebook from '../assets/facebook.svg'
 import Instagram from '../assets/instagram.svg'
 import { Button } from 'reactstrap';
 
+
 export default function Invitation() {
     return (
-        <div className='page-wrapper'>
+        <div className='page-wrapper pt-4'>
             <div><img className='logo pt-4' src={Logo} alt='#' /></div>
-            <p className='text-white text-center display-4 font-weight-bold'>My name</p>
-            <p className='text-white text-center'>invite friends with your profile link so they can have fun with you.</p>
-            <p className='text-white text-center'>http://playroom.live/nickname</p>
+            <p className='text-white text-center display-4 font-weight-bold pt-4'>slickback</p>
+            <p className='text-white text-center pt-4'>invite friends with your profile link so they can have fun with you.</p>
+            <p className='text-white text-center pt-4'>http://playroom.live/slickback</p>
 
             <div>
                 <span className='p-3'>
@@ -20,11 +21,11 @@ export default function Invitation() {
                     </Button>
                 </span>
                 <span className='p-3'>
-                    <Button className='w-75 m-auto' color="primary" size="lg" block>
+                    <Button style={{background: '#4267b2'}} className='w-75 m-auto' size="lg" block>
                     <img className='social-icon' src={Facebook} alt='#' /> Facebook</Button>
                 </span>
                 <span className='p-3'>
-                    <Button className='w-75 m-auto bg-transparent' size="lg" block>
+                    <Button className='w-75 m-auto text-danger' color='light' size="lg" block>
                     <img className='social-icon' src={Instagram} alt='#' /> Instagram</Button>
                 </span>
             </div>
