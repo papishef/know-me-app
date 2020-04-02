08ed3d64b5df284a0156a09c67d3583822c594a7
veectorh <blackjetv5@gmail.com>
2020-04-02 01:16:39 +0100

    functional component refactoring for sign-in

--- client/src/views/SignIn.js
+++ client/src/views/SignIn.js
@@ -1,66 +1,74 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/playroom-logo.png'
 import { Container, Input, InputGroup, /*, InputGroupAddon, InputGroupText,*/ Button } from 'reactstrap'
 
-export class SignIn extends Component {
 
-    state = {
-        nickname: '',
-        sex: ''
-      }
-      handleChange = event => {
-        event.preventDefault();
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({
-          [name]: value    });
-      };
+// sign in component
+ const Signin = () => {
+    // generic state hooks for all input
+    const [input, setInput] = useState('')
+    
+    // function to handle input change
+    const handleChange = (e) => setInput({
+        ...input, [e.target.name]: e.target.value
+    })
 
-      handleSubmit = event => {
-    event.preventDefault();
+    // function to submit data
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(input)
+        // post request goes here
 
-    const user = {
-      nickname: this.state.nickname,
-      sex: this.state.sex
-    };
-    console.log(user)
-}
-    render() {
-        return (
-            <div className='page-wrapper'>
+        // reset state
+        
+    }
+    return (
+       <div className='page-wrapper'>
+           {/* content container */}
                 <Container className="themed-container login-wrapper" >
+                    {/* logo */}
                     <div><img className='logo py-4' src={Logo} alt='#' /></div>
-                    <form className='pt-4 form-wrapper' onSubmit={this.handleSubmit}>
+
+                    {/* user form */}
+                    <form className='pt-4 form-wrapper' onSubmit={handleSubmit}>
+
                         <InputGroup className='pt-4'>
-                            <Input className='bg-transparent text-white form-input' name='nickname' placeholder="Nickname" value={this.state.email} type="string"  onChange={this.handleChange} />
+                            <Input 
+                            className='bg-transparent text-white form-input' 
+                            name='username' placeholder="Nickname"  
+                            type="string"  onChange={handleChange} />
                         </InputGroup>
                         <br />
+
                         <InputGroup className='pt-4'>
-                            <Input className='bg-transparent text-white form-input' name='sex' placeholder="sex" value={this.state.email} type="string" onChange={this.handleChange}/>
+                            <Input 
+                            className='bg-transparent text-white form-input' 
+                            name='gender' placeholder="gender"  
+                            type="string" onChange={handleChange}/>
                         </InputGroup>
-                        {/* <InputGroup>
-                            <InputGroupAddon addonType="prepend">
-                            <InputGroupText>
-                                <Input addon type="checkbox" aria-label="Checkbox for following text input" />
-                            </InputGroupText>
-                            </InputGroupAddon>
-                        </InputGroup> */}
                         <br />
-                        <Button className='submit border border-light' type='submit' color="secondary">submit</Button>{' '}
+
+                        <Button 
+                        className='submit border border-light' 
+                        type='submit' 
+                        color="secondary">submit</Button>{' '}
                     </form>
+
+
+                    {/* additional information */}
                     <div className='text-white pt-4'>
                         <strong>Disclaimer:</strong>Nickname should be kept private,
                          it would be used to identify you when you log in and out of the game.
                     </div>
+
                     <div className='text-white pt-2'>
                         By submiting, I agree to your <strong>Terms of service</strong> and
                         <strong>Privacy Policy</strong>
                     </div>
+
                 </Container>
             </div>
-        )
-    }
+    )
 }
 
-export default SignIn
+export default Signin;
