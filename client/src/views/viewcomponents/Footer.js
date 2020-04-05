6242e263c254132022e1c1f21a4df67b0f9fd6a9
papishef <sheriff.malebo@gmail.com>
2020-04-05 21:10:14 +0100

    maybe a fullstop

--- client/src/views/viewcomponents/Footer.js
+++ client/src/views/viewcomponents/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer style={{position: "relative", top: 30, textAlign: "center", color: "white", fontSize: 12, fontFamily: "Garamond", fontWeight: "bolder"}}>
-      <p>ⓒ {year} with ❤️ by Kyiakyia </p>
+      <p>ⓒ {year} with ❤️ by Kyiakyia. </p>
     </footer>
   );
 }
