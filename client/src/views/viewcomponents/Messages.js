c667cb099bdc1d779e1d11f0ea8db975bd9b5c40
veectorh <blackjetv5@gmail.com>
2020-04-07 00:18:56 +0100

    auto scroll bug fix

--- client/src/views/viewcomponents/Messages.js
+++ client/src/views/viewcomponents/Messages.js
@@ -1,15 +1,22 @@
 //jshint esversion: 6
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import ScrollToBottom from 'react-scroll-to-bottom'
 import Message from './Message';
 
 
+
 const Messages = ({ messages, nickname}) => {
+
+    const messagesEndRef = useRef(null);
+    const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+  useEffect(scrollToBottom, [messages]);
+
     return (
         <div className='message-container'>
-            <ScrollToBottom>
-                {messages.map((message, i) => <div key={i}><Message message={message} nickname={nickname} /></div>)}
-            </ScrollToBottom>
+            {messages.map((message, i) => <div key={i}><Message message={message} nickname={nickname} /></div>)}
+            <div ref={messagesEndRef} />
         </div>
     )
 }
