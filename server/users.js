c260758808ed4c5b3c6c4947d639f077539288d3
papishef <sheriff.malebo@gmail.com>
2020-04-06 15:56:34 +0100

    a few semi-colons

--- server/users.js
+++ server/users.js
@@ -18,13 +18,13 @@ const addUser = ({ id, nickname, roomID }) => {
   users.push(user);
 
   return { user };
-}
+};
 
 const removeUser = (id) => {
   const index = users.findIndex((user) => user.id === id);
 
   if (index !== -1) return users.splice(index, 1)[0];
-}
+};
 
 const getUser = (id) => users.find((user) => user.id === id);
 
