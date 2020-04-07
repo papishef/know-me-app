8ed8736e44d25d62489bb4af5ce03442c53b176c
veectorh <blackjetv5@gmail.com>
2020-04-07 10:06:20 +0100

    update invitation style

--- client/src/views/Invitation.js
+++ client/src/views/Invitation.js
@@ -4,7 +4,7 @@ import Logo from '../assets/playroom-logo.png'
 import Whatsapp from '../assets/whatsapp.svg'
 import Facebook from '../assets/facebook.svg'
 import Instagram from '../assets/instagram.svg'
-import { NavLink, Col, Row } from 'reactstrap';
+import { NavLink, Button } from 'reactstrap';
 
 
 export default function Invitation() {
@@ -15,30 +15,31 @@ export default function Invitation() {
             <p className='text-white text-center pt-4'>invite friends with your profile link so they can have fun with you.</p>
             <p className='text-white text-center pt-4'>http://playroom.live/</p>
 
-            <div>
-                <Row xs='3'>
-                    <Col>
-                        <span className='p-3'>
-                            <NavLink className='text-light' href="https://wa.me/?text=I'm%20interested%20in%20your%20car%20for%20sale" target='blank'>
-                                <img className='social-icon' src={Whatsapp} alt='#' />
-                            </NavLink>
-                        </span>
-                    </Col>
-                    <Col>
-                        <span className='p-3'>
-                            <NavLink className='text-light' href="http://m.me/mybot?ref=joinmyroom"  target='blank'>
-                                <img className='social-icon' src={Facebook} alt='#' />
-                            </NavLink>
-                        </span>
-                    </Col>
-                    <Col>
-                        <span className='p-3'>
-                            <NavLink href="http://instagram.com/_u/" target='blank'>
-                                <img className='social-icon' src={Instagram} alt='#' />
-                            </NavLink>
-                        </span>
-                    </Col>
-                </Row>
+            <div style={{maxWidth: '80vw', margin: 'auto'}}>  
+                <span className='p-4'>  
+                    <Button style={{background: 'white'}} block>
+                        <NavLink className='text-dark' href="https://wa.me/?text=I'm%20interested%20in%20your%20car%20for%20sale" target='blank'>
+                            <img className='social-icon' src={Whatsapp} alt='#' />
+                            Whatsapp
+                        </NavLink>
+                    </Button>
+                </span>
+                <span className='p-4'>
+                    <Button className='bg-primary' block>
+                        <NavLink className='text-light' href="http://m.me/mybot?ref=joinmyroom"  target='blank'>
+                            <img className='social-icon' src={Facebook} alt='#' />
+                            Facebook
+                        </NavLink>
+                    </Button>
+                </span>
+                <span className='p-4'>
+                    <Button style={{background: 'transparent', border: 'solid 2px pink'}} block>
+                        <NavLink className='text-light' href="http://instagram.com/_u/" target='blank'>
+                            <img className='social-icon' src={Instagram} alt='#' />
+                            Instagram
+                        </NavLink>
+                    </Button>
+                </span>
             </div>
         </div>
     )
