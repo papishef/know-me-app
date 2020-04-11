ce2300362244ea752c056f05f33ca9b6c5b92eea
papishef <sheriff.malebo@gmail.com>
2020-04-11 17:51:01 +0100

    static path added

--- app.js
+++ app.js
@@ -404,8 +404,7 @@ app.get("/", (req, res) => {
 });
 
 
-app.get('/', function (req, res) {
+
 app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
-});
