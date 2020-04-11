ef16cdf673516eaeb0263106a0c18b30e205bac8
papishef <sheriff.malebo@gmail.com>
2020-04-11 12:34:33 +0100

    another cors trial

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Accept"
   );
   next();
 });
