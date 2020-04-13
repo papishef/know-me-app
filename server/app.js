3daa4ab6d160f8aa4bef1021ab6fc8f685315fd7
veectorh <blackjetv5@gmail.com>
2020-04-13 15:47:22 +0100

    testing live

--- server/app.js
+++ server/app.js
@@ -29,19 +29,19 @@ const {
 
 const allQuestions = require("./questions");
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE, PATCH");
-    return res.status(200).json({});
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE, PATCH");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 app.use(cors());
 
