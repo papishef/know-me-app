ce58db264dd210cc9122829b165eb04ea0201adf
papishef <sheriff.malebo@gmail.com>
2020-04-24 16:28:57 +0100

    messages test2

--- client/src/views/chat.js
+++ client/src/views/chat.js
@@ -87,7 +87,7 @@ useEffect(() => {
      });
      if (messages.length > 3) {
         //  setLoading(true);
-         setMessages(messages = [messages[messages.length - 1]]);
+         setMessages(messages[messages.length - 1]);
 
         axios.get(`https://limitless-river-10398.herokuapp.com/chat/${roomID}`)
         .then(response => {
