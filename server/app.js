b80a107d2eb33ebd83930d1709af0cedf7b1a7f0
papishef <sheriff.malebo@gmail.com>
2020-04-27 17:18:05 +0100

    useEffect alteration test

--- server/app.js
+++ server/app.js
@@ -75,14 +75,14 @@ app.use(passport.session());
 
 // Database Connection
 mongoose.connect("mongodb+srv://admin-sheriff:Surprise1%40@kyiakyiadigital-c6jba.mongodb.net/PlayRoomDB", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-.catch((error) => {
-  console.log(error);
-});
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 
 /////DEFINITION OF USER SCHEMA////////////////
@@ -208,6 +208,11 @@ io.on('connection', function (socket) {
   //Expecting a message to be sent 
   socket.on("sendMessage", (message, roomID, callback) => {
     const user = getUser(socket.id);
+    //send message to room users
+    io.to(user.roomID).emit("message", {
+      user: user.nickname,
+      text: message
+    });
     ///save message to history
     let messageHistory = new Chat({
       message: message,
@@ -216,18 +221,18 @@ io.on('connection', function (socket) {
     });
     messageHistory.save((error) => {
       if (error) // ...
-      console.log(error);
-    });
-    //send message to room users
-    io.to(user.roomID).emit("message", {
-      user: user.nickname,
-      text: message
+        console.log(error);
     });
     callback();
   });
   //Expecting a question to be sent
   socket.on("sendQuestion", (quest, roomID, callback) => {
     const userQuest = getUser(socket.id);
+    //send question to room users
+    io.to(userQuest.roomID).emit("quest", {
+      user: userQuest.nickname,
+      text: quest
+    });
     //save questions to chats schema
     let questionHistory = new Chat({
       message: quest,
@@ -236,12 +241,7 @@ io.on('connection', function (socket) {
     });
     questionHistory.save((error) => {
       if (error) // ...
-      console.log(error);
-    });
-    //send question to room users
-    io.to(userQuest.roomID).emit("quest", {
-      user: userQuest.nickname,
-      text: quest
+        console.log(error);
     });
     callback();
   });
@@ -258,7 +258,7 @@ io.on('connection', function (socket) {
     });
     questForCalc.save((error) => {
       if (error) // ...
-      console.log(error);
+        console.log(error);
     });
   });
 
